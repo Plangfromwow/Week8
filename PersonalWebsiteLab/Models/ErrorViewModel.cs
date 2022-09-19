ce16381235893266837385295dee771cff710b76
Jacob Garcia <jacobgarcia@rocketmortgage.com>
2022-09-19 13:05:53 -0400

    after lunch commit

---
+++ PersonalWebsiteLab/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PersonalWebsiteLab.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
