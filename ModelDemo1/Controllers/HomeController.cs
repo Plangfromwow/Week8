1e6bcb98bfe23883cbbc6d7607a2e2bf4eb88585
Jacob Garcia <jacobgarcia@rocketmortgage.com>
2022-09-21 14:49:40 -0400

    pre-lab push to get all the things up

--- ModelDemo1/Controllers/HomeController.cs
+++ ModelDemo1/Controllers/HomeController.cs
@@ -25,13 +25,39 @@ namespace ModelDemo1.Controllers
 
         public IActionResult Register(Person per)
         {
-            return View(per);
+            if (per.Born > 2004)
+            {
+                // let's use a different view for this one because they are young 
+                return View("Under18");
+            }
+            else
+            {
+                return View(per);
+            }
+            
         }
 
         public IActionResult Sample()
         {
             Person fred = new Person() { FirstName ="Fred",LastName="Franklin",Email="Banana@yahoo.com",Born=1970};
             return View(fred);
+        }
+
+        public IActionResult SampleList()
+        {
+            List<Person> people = new List<Person>();
+
+            Person fred = new Person() { FirstName = "Fred", LastName = "Franklin", Email = "Banana@yahoo.com", Born = 1970 };
+            Person per = new Person() { FirstName = "Greg", LastName = "Thompson", Email = "Banana@yahoo.com", Born = 1170 };
+            people.Add(per);
+            people.Add(fred);
+            people.Add(new Person() { FirstName="Sally",LastName="Henderson",Email="youhoo@youhoo.esg",Born=156});
+
+            ViewData["Department"] = "IT";
+            ViewData["Year"] = "IT";
+
+            return View(people);
+
         }
 
         public IActionResult ShowRegister()
---
+++ Practice1/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Practice1.Models;
+using System.Diagnostics;
+
+namespace Practice1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult AddPetForm()
+        {
+
+            return View();
+        }
+
+        public IActionResult PetInfo(Animal pet)
+        {
+            return View(pet);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
